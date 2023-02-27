aad78cecbfcda40d3b93568e989a3fcd4193fe36
emteeyaz hussain <77221528+emteeyazhussain@users.noreply.github.com>
2023-02-27 16:13:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,9 @@ def index():
 
     else:
         return render_template('index.html')
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8000, debug=True)
+#app.run(debug=True)
 
-
-if __name__=="__main__":
-    app.run(host="0.0.0.0")
+# if __name__=="__main__":
+#     app.run(host="0.0.0.0")
